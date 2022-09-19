312847066e91fef7f83ef0d7269cbd36a45ee8b5
eric <eholdener12@gmail.com>
2022-09-18 17:52:01 -0700

    may be shelving this for a while, learning react might not be hte best implementation for an idle game

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,7 +8,7 @@ export default function Sidebar() {
   return (
     <>
       <SidebarItem name="Character" image={CharacterImage} link="/" skill={false} character={true}/>
-      <SidebarItem name="Inventory" image={ShirtImage} link="/inventory" skill={false} character={false}/>
+      <SidebarItem name="Inventory" image={ShirtImage} link="/inventory/equip" skill={false} character={false}/>
       <SidebarItem name="Combat" image={SwordSprite} link="/combat" skill={false} character={false}/>
       <SidebarItem name="Potions" image={RedPotionSprite} link="/brewing" skill={true} character={false}/>
     </>
