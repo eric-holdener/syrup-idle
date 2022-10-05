0725e952406e0c9b1cd8bbc9891b7be15afb62db
EHoldener <eholdener12@gmail.com>
2022-10-05 04:43:53 -0700

    protected all routes when there is no character selected, redirects to character selection screen

--- src/App.js
+++ src/App.js
@@ -10,27 +10,28 @@ import Inventory from './components/Character/Inventory';
 import EquipmentTab from './components/Character/inventory/EquipmentTab';
 import UseTab from './components/Character/inventory/UseTab';
 import EtcTab from './components/Character/inventory/EtcTab';
+import LoginSplash from './components/Login/LoginSplash';
+import AuthChecker from './components/Route_Protection/AuthChecker';
 
 function App() {
   return (
-    <>
-      <Provider store={Store}>
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Character />} />
-                <Route path="combat" element={<CombatMain />} />
-                <Route path="brewing" element={<BrewingMain />} />
-                <Route path="inventory" element={<Inventory />}>
-                  <Route path="equip" element={<EquipmentTab />} />
-                  <Route path="use" element={<UseTab />} />
-                  <Route path="etc" element={<EtcTab />} />
-                </Route>
+    <Provider store={Store}>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<AuthChecker><Character /></AuthChecker>} />
+              <Route path="combat" element={<AuthChecker><CombatMain /></AuthChecker>} />
+              <Route path="brewing" element={<AuthChecker><BrewingMain /></AuthChecker>} />
+              <Route path="inventory" element={<AuthChecker><Inventory /></AuthChecker>}>
+                <Route path="equip" element={<AuthChecker><EquipmentTab /></AuthChecker>} />
+                <Route path="use" element={<AuthChecker><UseTab /></AuthChecker>} />
+                <Route path="etc" element={<AuthChecker><EtcTab /></AuthChecker>} />
               </Route>
-            </Routes>
-          </BrowserRouter>
-      </Provider>
-    </>
+              <Route path="character-select" element={<LoginSplash />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </Provider>
   )
 }
 
---
+++ src/components/Login/LoginSplash.js
@@ -0,0 +1,7 @@
+export default function LoginSplash() {
+  return (
+    <>
+      <p>Login splash</p>
+    </>
+  )
+}
