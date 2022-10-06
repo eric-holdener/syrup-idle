d61fe6f168e0f152022d49acb7ae2b18752d2271
EHoldener <eholdener12@gmail.com>
2022-10-06 09:07:22 -0700

    save game loading might work now, no way to know until we have data in local storage

--- src/components/Login/LoginSplash.js
+++ src/components/Login/LoginSplash.js
@@ -5,18 +5,33 @@ export default function LoginSplash() {
 
   const [characters, setCharacters] = useState(() => {
     const saved = localStorage.getItem("savegames");
-    let savegames = {}
+    let savegames = {
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null
+    };
+
     if (saved) {
       const initialValue = JSON.parse(saved);
-    } else {
-      savegames = {
-        1: null,
-        2: null,
-        3: null,
-        4: null,
-        5: null
-      }
-    }
+      if (initialValue[1]) {
+        savegames[1] = initialValue[1]
+      };
+      if (initialValue[2]) {
+        savegames[2] = initialValue[2]
+      };
+      if (initialValue[3]) {
+        savegames[3] = initialValue[3]
+      };
+      if (initialValue[4]) {
+        savegames[4] = initialValue[4]
+      };
+      if (initialValue[5]) {
+        savegames[5] = initialValue[5]
+      };
+    };
+
     return savegames;
   });
 
