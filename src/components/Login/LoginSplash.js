21c804346fa1e9b3f1c27498523deea13436996a
EHoldener <eholdener12@gmail.com>
2022-10-06 08:58:34 -0700

    created a notes file, started work on savegames in localstorage, created a default character helper file

--- src/components/Login/LoginSplash.js
+++ src/components/Login/LoginSplash.js
@@ -1,6 +1,25 @@
 import CharacterBox from "./CharacterBox";
+import { useState } from "react";
 
 export default function LoginSplash() {
+
+  const [characters, setCharacters] = useState(() => {
+    const saved = localStorage.getItem("savegames");
+    let savegames = null
+    if (saved) {
+      const initialValue = JSON.parse(saved);
+    } else {
+      savegames = {
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {}
+      }
+    }
+    return savegames;
+    console.log(saved)
+  })
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       <CharacterBox />
---
+++ src/helpers/DefaultCharacter.js
@@ -0,0 +1,43 @@
+export default function defaultCharacter() {
+  const default_char = {
+    currentHp: 10,
+    name: "Teidan",
+    job: "Warrior",
+    level: 1,
+    maxHp: 50,
+    maxMp: 10,
+    currentMp: 10,
+    exp: 0,
+    availableAp: 0,
+    str: 5,
+    dex: 5,
+    int: 5,
+    luk: 5,
+    xpTNL: 1144,
+    inventory: {},
+    equipment: {
+      cap: null,
+      medal: null,
+      forehead: null,
+      ring_1: null,
+      ring_2: null,
+      ring_3: null,
+      ring_4: null,
+      eye_acc: null,
+      ear_acc: null,
+      mantle: null,
+      chest: null,
+      pendant: null,
+      shoulder: null,
+      shield: null,
+      gloves: null,
+      pants: null,
+      belt: null,
+      shoes: null
+    },
+    equip_inventory: {},
+    use_inventory: {},
+    etc_inventory: {}
+  }
+  return 
+}
