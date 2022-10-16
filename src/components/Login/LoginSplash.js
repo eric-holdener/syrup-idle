35f2454b11f397c7910c5c0e6bf4347ae660e066
eric <eholdener12@gmail.com>
2022-10-16 07:25:23 -0700

    refactored login screen to display every saved character and an extra box to make a new character

--- src/components/Login/LoginSplash.js
+++ src/components/Login/LoginSplash.js
@@ -5,31 +5,13 @@ export default function LoginSplash() {
 
   const [characters, setCharacters] = useState(() => {
     const saved = localStorage.getItem("savegames");
-    let savegames = {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null
-    };
-
+    let savegames = []
     if (saved) {
       const initialValue = JSON.parse(saved);
-      if (initialValue[1]) {
-        savegames[1] = initialValue[1]
-      };
-      if (initialValue[2]) {
-        savegames[2] = initialValue[2]
-      };
-      if (initialValue[3]) {
-        savegames[3] = initialValue[3]
-      };
-      if (initialValue[4]) {
-        savegames[4] = initialValue[4]
-      };
-      if (initialValue[5]) {
-        savegames[5] = initialValue[5]
-      };
+      let save = 0
+      for (save; save < initialValue.length; save++) {
+        savegames.append([saved[save], save]);
+      }
     };
 
     return savegames;
@@ -37,11 +19,8 @@ export default function LoginSplash() {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <CharacterBox char={characters[1]} id="1"/>
-      <CharacterBox char={characters[2]} id="2"/>
-      <CharacterBox char={characters[3]} id="3"/>
-      <CharacterBox char={characters[4]} id="4"/>
-      <CharacterBox char={characters[5]} id="5"/>
+      {characters.map((character) => <CharacterBox char={character[0]} id={character[1]}/>)}
+      <CharacterBox char={null} id={characters.length}/>
     </div>
   )
 }
