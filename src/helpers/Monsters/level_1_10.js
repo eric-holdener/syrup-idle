d1ae239852cd3e0b41c34293b21ee5bd1142df47
eric <eholdener12@gmail.com>
2022-10-26 19:08:16 -0700

    created blue snail archetype

--- src/helpers/Monsters/level_1_10.js
+++ src/helpers/Monsters/level_1_10.js
@@ -6,3 +6,9 @@ function Snail() {
 
   return MonsterCreator("Snail", 3, drops, 4, 6, -65, 20, 0, 22, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 1)
 }
+
+function BlueSnail() {
+  drops = DropCreator();
+
+  return MonsterCreator("Blue Snail", 4, drops, 8, 12, -50, 20, 0, 27, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 2)
+}
