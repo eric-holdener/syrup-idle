0f9579493f496d7e5c7b3af6eeb81d96612adcbb
eric <eholdener12@gmail.com>
2022-10-26 19:12:05 -0700

    created red snail, updated checklist and notes

--- src/helpers/Monsters/level_1_10.js
+++ src/helpers/Monsters/level_1_10.js
@@ -4,11 +4,17 @@ import DropCreator from "../DropCreator";
 function Snail() {
   drops = DropCreator();
 
-  return MonsterCreator("Snail", 3, drops, 4, 6, -65, 20, 0, 22, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 1)
+  return MonsterCreator("Snail", 3, drops, 4, 6, -65, 20, 0, 22, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 1);
 }
 
 function BlueSnail() {
   drops = DropCreator();
 
-  return MonsterCreator("Blue Snail", 4, drops, 8, 12, -50, 20, 0, 27, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 2)
+  return MonsterCreator("Blue Snail", 4, drops, 8, 12, -50, 20, 0, 27, 0, 0, 0, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 2);
+}
+
+function RedSnail() {
+  drops = DropCreator();
+
+  return MonsterCreator("Red Snail", 8, drops, 12, 18, -50, 30, 0, 35, 0, 3, 10, 0, 0, null, 0, 0, [], [1, 2, 3, 4, 5], [], 5);
 }
