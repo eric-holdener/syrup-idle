f0a71c7c8dd89b65552584b0401a37f3625e69f9
eric <eholdener12@gmail.com>
2022-10-26 19:28:59 -0700

    formatting

--- src/helpers/Locations/Victoria Island/Lith Harbor/Locations.js
+++ src/helpers/Locations/Victoria Island/Lith Harbor/Locations.js
@@ -4,11 +4,10 @@ import MonstersLvl1To10 from "../../../Monsters/level_1_10";
 let monsters_1_10 = MonstersLvl1To10();
 
 function Thicket_Around_Beach_1() {
-  return DefaultLocation("Thicket Around the Beach 1", [
-    monsters_1_10[red_snail],
-    monsters_1_10[snail],
-    monsters_1_10[blue_snail]
-  ])
+  return DefaultLocation(
+    "Thicket Around the Beach 1", 
+    [monsters_1_10[red_snail], monsters_1_10[snail], monsters_1_10[blue_snail]]
+  )
 }
 
 export default function LithHarborLocations() {
