9606246f270550276911af7e769393c67b80d6e3
eric <eholdener12@gmail.com>
2022-11-19 21:25:34 -0800

    fixed inventory page

--- src/components/Character/Inventory.js
+++ src/components/Character/Inventory.js
@@ -9,30 +9,13 @@ export default function Inventory() {
   const character = useSelector((state) => state.character);
   const [ActiveInventory, SetActiveInventory] = useState(0);
 
-  function DisplayAllInventory(inventory) {
-    for (let i = 0; i < inventory.length; i++) {
-      return (
-        <InventoryBox boxnumber={i} setActive={SetActiveInventory}/> 
-      )
-    }
-  }
-
-  function DisplayActiveInventory(inventory) {
-    const display_active = inventory[ActiveInventory];
-    for (let i = 0; i < display_active.length; i++) {
-      return (
-        <ItemBox item={display_active[i]} />
-      )
-    }
-  }
-
   return (
     <>
       <div className="flex justify-start w-full p-3 gap-3">
-        {DisplayAllInventory(character.inventory)}
+        {character.inventory.map((slot, index) => <InventoryBox boxnumber={index} setActive={SetActiveInventory} />)}
       </div>
       <div>
-        {DisplayActiveInventory(character.inventory)}
+        {character.inventory[ActiveInventory].map((item) => <ItemBox item={item} />)}
       </div>
     </>
 
