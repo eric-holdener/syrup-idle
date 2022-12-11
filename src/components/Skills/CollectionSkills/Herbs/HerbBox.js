ab561688d3ce0bb5ef6acf0058f8fcce4f5a7d00
EHoldener <eholdener12@gmail.com>
2022-12-11 13:22:29 -0800

    more styling

--- src/components/Skills/CollectionSkills/Herbs/HerbBox.js
+++ src/components/Skills/CollectionSkills/Herbs/HerbBox.js
@@ -1,7 +1,7 @@
 export default function HerbBox(props) {
   return (
-    <button onClick={() => props.setHerbTraining(props.herb)} className="w-1/6 h-1/6 bg-slate-400">
-      <p>{props.herb.name}</p>
+    <button onClick={() => props.setHerbTraining(props.herb)} className="w-1/6 h-1/6 bg-slate-800 opacity-75 hover:opacity-100">
+      <p className="text-white">{props.herb.name}</p>
     </button>
   )
 }
--- src/components/Skills/CollectionSkills/Herbs/HerbHeader.js
+++ src/components/Skills/CollectionSkills/Herbs/HerbHeader.js
@@ -2,8 +2,8 @@ import StopTraining from "../../../../helpers/StopTrainingButton"
 
 export default function HerbHeader() {
   return (
-    <div className="w-full h-1/6">
-      <p>Herb Header</p>
+    <div className="w-full h-1/6 bg-slate-800 opacity-75 mb-3">
+      <p className="text-white">Herb Header</p>
       <StopTraining />
     </div>
   )
