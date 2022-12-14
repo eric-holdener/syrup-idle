5f57305bafcf7dfe6f1eb9b322b2edb4c4756b92
EHoldener <eholdener12@gmail.com>
2022-12-14 09:39:32 -0800

    fishing header shell created

--- src/components/Skills/CollectionSkills/Fishing/FishHeader.js
+++ src/components/Skills/CollectionSkills/Fishing/FishHeader.js
@@ -1,5 +1,21 @@
+import StopTraining from "../../../../helpers/StopTrainingButton"
+// import TimerBar from "../../../../helpers/TimerBar"
+// import { useSelector } from "react-redux"
+// import EmptyTimerBar from "../../../../helpers/EmptyTimerBar";
+
 export default function FishHeader() {
+  // const character = useSelector((state) => state.character);
+  // const training = character.currently_training;
+
   return (
-    <p>Fish Header</p>
+    <div className="w-full h-1/6 bg-slate-800 opacity-75 mb-3">
+      {/* {training ? (
+        <TimerBar />
+      ) : (
+        <EmptyTimerBar />
+      )} */}
+      <p className="text-white">Fish Header</p>
+      <StopTraining />
+    </div>
   )
 }
