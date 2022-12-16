08f706deb1238acafa253c6ebbc11721b9a65532
EHoldener <eholdener12@gmail.com>
2022-12-16 15:49:11 -0800

    working on smithing boxes - determining if an item can be smithed and rendering conditionally based on that

--- src/components/Skills/RefiningSkills/Smithing/SmithingBox.js
+++ src/components/Skills/RefiningSkills/Smithing/SmithingBox.js
@@ -1,5 +1,43 @@
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
 export default function SmithingBox(props) {
+  const character = useSelector((state) => state.character);
+  const valid_craft_arr = new Array(props.recipe.items_req.length).fill(false);
+  const [validCraft, setValidCraft] = useState(false);
+  let valid_craft_arr_iterator = 0;
+
+  function allTrue(arr) {
+    return arr.every(element => element === true);
+  }
+
+  console.log(character);
+  console.log(valid_craft_arr);
+
+  function determineValidity() {
+    for (const [key, value] of Object.entries(character.etcInventory)) {
+      if (props.recipe.items_req.includes(parseInt(key))) {
+        valid_craft_arr[valid_craft_arr_iterator] = true;
+        console.log(valid_craft_arr);
+        valid_craft_arr_iterator += 1;
+        if (allTrue(valid_craft_arr)) {
+          console.log("all true")
+          setValidCraft(true);
+          break;
+        }
+      }
+    };
+  }
+
   return (
-    <p>Smithing Box</p>
+    <>
+      {determineValidity()}
+      {validCraft ? (
+        <p>Smithing Box</p>
+      ) : (
+        <p>not valid</p>
+      )}
+      <button onClick={() => console.log(validCraft)}>test valid craft</button>
+    </>
   )
 }
--- src/helpers/TickSystem.js
+++ src/helpers/TickSystem.js
@@ -12,24 +12,24 @@ export default function TickSystem() {
       console.log(training);
       console.log(training.skill_tree)
       if (training.skill_tree === "gathering" || training.skill_tree === "mining" || training.skill_tree === "fishing") {
-        console.log("here");
+        for (const item of Object.values(training.item.items)) {
+          const randomQuantity = item.drop_range[Math.floor(Math.random() * item.drop_range.length)];
+          if (randomQuantity > 0) {
+            const payload = {
+              type: item.type,
+              item: {
+                quantity: randomQuantity,
+                id: item.id,
+                name: item.name,
+              }
+            }
+            dispatch(addItem(payload));
+          }
+        }
       } else if (training.skill_tree === "smithing") {
         console.log("there");
       }
-      // for (const item of Object.values(training.item.items)) {
-      //   const randomQuantity = item.drop_range[Math.floor(Math.random() * item.drop_range.length)];
-      //   if (randomQuantity > 0) {
-      //     const payload = {
-      //       type: item.type,
-      //       item: {
-      //         quantity: randomQuantity,
-      //         id: item.id,
-      //         name: item.name,
-      //       }
-      //     }
-      //     dispatch(addItem(payload));
-      //   }
-      // }
+ 
     }, training.item.time);
 
   return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
