c2ace883b149ef29738796afeb3df2ba47fbf1ba
EHoldener <eholdener12@gmail.com>
2022-12-22 11:31:05 -0800

    started implementinig craftable and buyable potions

--- src/components/Shop/AllBuyablePotions.js
+++ src/components/Shop/AllBuyablePotions.js
@@ -1,25 +1,49 @@
 // id range 82 - 
 // buyable potions are healing aspects (mostly)
-// heal types:
-// 1 - flat amount
-// 2 - percentage based (percentage depicted as whole number)
+// potion types:
+// 1 - flat amount healing
+// 2 - percentage based healing (percentage depicted as whole number)
+// 3 - other
+// healing types:
+// 1 - hp
+// 2 - mp
 
 const red_potion = {
   id: 82,
   name: "Red Potion",
   type: 2,
   skill: null,
-  heal_type: 1,
-  heal_amount: 50
+  potion_type: 1,
+  heal_amount: 50,
+  cost: 100,
+  level_req: 1,
+  healing_type: 1
 }
 
-const power_elixir = {
+const barbarian_elixir = {
   id: 83,
-  name: "Power Elixir",
+  name: "Barbarian Elixir",
   type: 2,
   skill: null,
-  heal_type: 2,
-  heal_amount: 100
+  potion_type: 1,
+  heal_amount: 1500,
+  cost: 500,
+  level_req: 1,
+  healing_type: 1
 }
 
-const All_Buyable_Potion = [red_potion, power_elixir]
+const blue_potion = {
+  id: 84,
+  name: "Blue Potion",
+  type: 2,
+  skill: null,
+  potion_type: 1,
+  heal_amount: 100,
+  cost: 100,
+  level_req: 1,
+  healing_type: 2
+}
+
+
+
+const All_Buyable_Potion = [red_potion]
--- src/components/Skills/CraftingSkills/Alchemy/AllCraftablePotions.js
+++ src/components/Skills/CraftingSkills/Alchemy/AllCraftablePotions.js
@@ -1,2 +1,76 @@
 // id ranges xx - xx
 // craftable potions boost aspects, like exp or att
+// potion types:
+// 1 - flat amount healing
+// 2 - percentage based healing (percentage depicted as whole number)
+// 3 - other
+
+const all_cure_potion = {
+  // id: xx,
+  name: "All Cure Potion",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - clears abnormal statuses
+}
+
+const antidote = {
+  // id: xx,
+  name: "Antidote",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - clears poison statuses
+}
+
+const angelic_steps = {
+  // id: xx,
+  name: "Angelic Steps",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - speed +5 for x ticks (10 min in maple)
+}
+
+const archer_elixer = {
+  // id: xx,
+  name: "Archer Elixer",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - avoidability +20
+}
+
+const blastrojuice = {
+  // id: xx,
+  name: "Blastrojuice",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - weapon att +90 for x ticks (1 min in maple)
+}
+
+const body_physics_medicine = {
+    // id: xx,
+    name: "Body and Physics Medicine",
+    type: 2,
+    skill: 5,
+    potion_type: 3,
+    items_req: []
+    // effect? - att +8 for x ticks (15 min in maple)
+}
+
+const dexterity_pill = {
+  // id: xx,
+  name: "Dexterity Pill",
+  type: 2,
+  skill: 5,
+  potion_type: 3,
+  items_req: []
+  // effect? - avoidability +10 for x ticks (10 min in maple)
+}
